bfa2734a804dcda9e463798977c7d249b87ff340
incatrex <incatrex@gmail.com>
2021-08-12 20:41:00 -0400

    attempt to add e2e test

---
+++ client/tests/e2e/specs/listing-restaurants.spec.js
@@ -0,0 +1,21 @@
+describe('Listing Restaurants', () => {
+    it('shows restaurants from the server', () => {
+        const sushiPlace = 'Sushi Place';
+        const pizzaPlace = 'Pizza Place';
+
+        cy.server({ force404: true });
+
+        cy.route({
+            method: 'GET',
+            url: 'https://outside-in-dev-api.herokuapp.com/UxRTJ2R3y25V15A5LtAkpDXRxivM01vd/restaurants',
+            response: [
+                { id: 1, name: sushiPlace },
+                { id: 2, name: pizzaPlace },
+            ],
+        });
+
+        cy.visit('/');
+        cy.contains(sushiPlace);
+        cy.contains(pizzaPlace);
+    });
+});
