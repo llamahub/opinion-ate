7bef5b7b2413562c91ba216568fe2606085a6b30
incatrex <incatrex@gmail.com>
2021-08-12 22:11:29 -0400

    refactor tests to reduce duplication

--- client/tests/unit/components/RestaurantList.spec.js
+++ client/tests/unit/components/RestaurantList.spec.js
@@ -1,60 +1,51 @@
 import Vuex from 'vuex';
-import { mount, createLocalVue } from '@vue/test-utils';
+import {mount, createLocalVue} from '@vue/test-utils';
 import RestaurantList from '@/components/RestaurantList';
 
 describe('RestaurantList', () => {
-    const localVue = createLocalVue();
-    localVue.use(Vuex);
+  const records = [
+    {id: 1, name: 'Sushi Place'},
+    {id: 2, name: 'Pizza Place'},
+  ];
 
-    it('loads restaurants on mount', () => {
-        const restaurantsModule = {
-            namespaced: true,
-            actions: {
-                load: jest.fn().mockName('load'),
-            },
-        };
-        const store = new Vuex.Store({
-            modules: {
-                    restaurants: restaurantsModule,
-            },
-        });
+  let restaurantsModule;
+  let wrapper;
 
-        mount(RestaurantList, { localVue, store });
-
-        expect(restaurantsModule.actions.load).toHaveBeenCalled();
+  beforeEach(() => {
+    restaurantsModule = {
+      namespaced: true,
+      state: {records},
+      actions: {
+        load: jest.fn().mockName('load'),
+      },
+    };
+    const store = new Vuex.Store({
+      modules: {
+        restaurants: restaurantsModule,
+      },
     });
 
-    it('displays the restaurants', () => {
-        const records = [
-            { id: 1, name: 'Sushi Place' },
-            { id: 2, name: 'Pizza Place' },
-        ];
+    wrapper = mount(RestaurantList, {localVue, store});
+  });
 
-        const restaurantsModule = {
-            namespaced: true,
-            state: { records },
-            actions: {
-                load: jest.fn().mockName('load'),
-            },
-        };
-        const store = new Vuex.Store({
-            modules: {
-                restaurants: restaurantsModule,
-            },
-        });
+  const localVue = createLocalVue();
+  localVue.use(Vuex);
 
-        const wrapper = mount(RestaurantList, { localVue, store });
+  it('loads restaurants on mount', () => {
+    expect(restaurantsModule.actions.load).toHaveBeenCalled();
+  });
 
-        const firstRestaurantName = wrapper
-            .findAll('[data-testid="restaurant"]')
-            .at(0)
-            .text();
-        expect(firstRestaurantName).toBe('Sushi Place');
+  it('displays the restaurants', () => {
+    const firstRestaurantName = wrapper
+      .findAll('[data-testid="restaurant"]')
+      .at(0)
+      .text();
+    expect(firstRestaurantName).toBe('Sushi Place');
 
-        const secondRestaurantName = wrapper
-            .findAll('[data-testid="restaurant"]')
-            .at(1)
-            .text();
-        expect(secondRestaurantName).toBe('Pizza Place');
-    });
+    const secondRestaurantName = wrapper
+      .findAll('[data-testid="restaurant"]')
+      .at(1)
+      .text();
+    expect(secondRestaurantName).toBe('Pizza Place');
+  });
 });
